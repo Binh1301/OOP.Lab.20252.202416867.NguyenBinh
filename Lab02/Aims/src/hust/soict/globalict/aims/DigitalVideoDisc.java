45c92d1f7267875ff8f658de3333cda07cc2aed4
Binh1301 <evergarden130106@gmail.com>
2026-04-12 21:19:26 +0700

    bai 9

--- Lab02/Aims/src/hust/soict/globalict/aims/DigitalVideoDisc.java
+++ Lab02/Aims/src/hust/soict/globalict/aims/DigitalVideoDisc.java
@@ -1,11 +1,26 @@
 package hust.soict.globalict.aims;
 
 public class DigitalVideoDisc {
+	private static int nbDigitalVideoDiscs = 0;
+	private final int id;
 	private String title;
 	private String category;
 	private String director;
 	private int lenghth;
 	private float cost;
+
+	private static int generateId() {
+		nbDigitalVideoDiscs++;
+		return nbDigitalVideoDiscs;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public static int getNbDigitalVideoDiscs() {
+		return nbDigitalVideoDiscs;
+	}
 	public String getTitle() {
 		return title;
 	}
@@ -37,20 +52,24 @@ public class DigitalVideoDisc {
 		this.cost = cost;
 	}
 	public DigitalVideoDisc(String title) {
+	    this.id = generateId();
 	    this.title = title;
 	}
 	public DigitalVideoDisc(String category, String title, float cost) {
+	    this.id = generateId();
 	    this.category = category;
 	    this.title = title;
 	    this.cost = cost;
 	}
 	public DigitalVideoDisc(String director, String category, String title, float cost) {
+	    this.id = generateId();
 	    this.director = director;
 	    this.category = category;
 	    this.title = title;
 	    this.cost = cost;
 	}
 	public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
+	    this.id = generateId();
 	    this.title = title;
 	    this.category = category;
 	    this.director = director;
