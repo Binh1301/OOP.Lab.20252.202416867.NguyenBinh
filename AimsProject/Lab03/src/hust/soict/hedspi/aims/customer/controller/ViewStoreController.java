a281d64ada31803cc6524cc7927f11a975435c62
Binh1301 <evergarden130106@gmail.com>
2026-06-05 16:32:52 +0700

    Design Store.fxml 5.1

---
+++ AimsProject/Lab03/src/hust/soict/hedspi/aims/customer/controller/ViewStoreController.java
@@ -0,0 +1,10 @@
+package hust.soict.hedspi.aims.customer.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class ViewStoreController {
+    @FXML
+    private void btnViewCartPressed(ActionEvent e) {
+    }
+}
